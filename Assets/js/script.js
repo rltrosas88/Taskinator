3f38f5583d4b8af0f2837604ba1d8ba3149b92f8
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-03-10 21:03:00 -0600

    2.5 Handle Form Submission

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
+//var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //buttonEl.addEventListener("click", function() {
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }//);
 
-buttonEl.addEventListener("click", createTaskHandler);
+//buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
